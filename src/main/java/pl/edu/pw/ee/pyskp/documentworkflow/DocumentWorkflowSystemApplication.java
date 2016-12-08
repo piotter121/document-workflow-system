29f2548c464d6a05462e45cfab1a1502badb60bc
Piotr Pyśk <defcon1994@gmail.com>
2016-12-08 16:00:52 +0100

    Utworzenie projektu Spring Boot

---
+++ src/main/java/pl/edu/pw/ee/pyskp/documentworkflow/DocumentWorkflowSystemApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.pw.ee.pyskp.documentworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentWorkflowSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentWorkflowSystemApplication.class, args);
+	}
+}
