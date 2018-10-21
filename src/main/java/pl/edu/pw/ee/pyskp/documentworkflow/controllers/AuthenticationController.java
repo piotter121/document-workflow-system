55f11274b92ae9cc754d121f6813e828f7453c80
Piotr Pyśk <defcon1994@gmail.com>
2018-10-21 23:40:31 +0200

    creating logger with lombok

--- src/main/java/pl/edu/pw/ee/pyskp/documentworkflow/controllers/AuthenticationController.java
+++ src/main/java/pl/edu/pw/ee/pyskp/documentworkflow/controllers/AuthenticationController.java
@@ -2,15 +2,13 @@ package pl.edu.pw.ee.pyskp.documentworkflow.controllers;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.edu.pw.ee.pyskp.documentworkflow.authentication.WebAuthenticationService;
-import pl.edu.pw.ee.pyskp.documentworkflow.dtos.NewUserDTO;
+import pl.edu.pw.ee.pyskp.documentworkflow.dtos.user.NewUserDTO;
 import pl.edu.pw.ee.pyskp.documentworkflow.exceptions.UserNotFoundException;
 import pl.edu.pw.ee.pyskp.documentworkflow.services.UserService;
 
@@ -18,12 +16,11 @@ import javax.validation.Valid;
 import java.util.Collections;
 import java.util.Map;
 
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
-    private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationController.class);
-
     @NonNull
     private final WebAuthenticationService authenticationService;
 
@@ -39,9 +36,9 @@ public class AuthenticationController {
                 return ResponseEntity.ok().body(Collections.singletonMap("token", token));
             }
         } catch (UserNotFoundException e) {
-            LOGGER.warn("User not found exception", e);
+            log.warn("User not found exception", e);
         } catch (Exception e) {
-            LOGGER.warn("Exception during get token", e);
+            log.warn("Exception during get token", e);
         }
 
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
