444c6ed74719f1cf3720e12b256b61699e48a09b
Piotr Pyśk <defcon1994@gmail.com>
2018-11-21 22:53:38 +0100

    App docker image build

--- src/test/java/pl/edu/pw/ee/pyskp/documentworkflow/DocumentWorkflowSystemApplicationTests.java
+++ src/test/java/pl/edu/pw/ee/pyskp/documentworkflow/DocumentWorkflowSystemApplicationTests.java
@@ -1,16 +1,24 @@
 package pl.edu.pw.ee.pyskp.documentworkflow;
 
+import org.cassandraunit.spring.CassandraUnit;
+import org.cassandraunit.spring.CassandraUnitDependencyInjectionTestExecutionListener;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@TestPropertySource("classpath:/application-test.properties")
+@TestExecutionListeners(listeners = CassandraUnitDependencyInjectionTestExecutionListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+@CassandraUnit
 @SpringBootTest
 public class DocumentWorkflowSystemApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
